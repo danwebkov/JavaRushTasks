038526aaef671fee3f9f3d03dc23c464c36942c0
danzel274 <danweb007@yandex.ru>
2025-01-29 11:02:49 +0300

    add additional tasks folder

---
+++ 1.JavaSyntax/src/additional/tasks/StringExamples.java
@@ -0,0 +1,10 @@
+package additional.tasks;
+
+public class StringExamples {
+    public static void main(String[] args) {
+        String str = "Hello World\n"
+                + "and doing fine!";
+
+        System.out.println(str);
+    }
+}
