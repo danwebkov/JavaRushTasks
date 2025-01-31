414e98702da0c101459cec6e99139045bb97c387
danzel274 <danweb007@yandex.ru>
2025-01-31 15:35:06 +0300

    add solution for task0305

---
+++ 1.JavaSyntax/src/com/javarush/task/pro/task03/task0305/Solution.java
@@ -0,0 +1,27 @@
+package com.javarush.task.pro.task03.task0305;
+
+import java.util.Scanner;
+
+/* 
+Три числа
+*/
+
+public class Solution {
+    public static void main(String[] args) {
+        //напишите тут ваш код
+
+        Scanner scan = new Scanner(System.in);
+        int a = scan.nextInt();
+        int b = scan.nextInt();
+        int c = scan.nextInt();
+
+        if (a == b && a == c)
+            System.out.println(a + " " + b + " " + c);
+        else if (a == b)
+            System.out.println(a + " " + b);
+        else if (a == c)
+            System.out.println(a + " " + c);
+        else if (b == c)
+            System.out.println(b + " " + c);
+    }
+}
