93f8aa5962e40b4bfde73919e2ad8c9abb31c159
danzel274 <danweb007@yandex.ru>
2025-02-04 18:50:27 +0300

    add solution for task0109

---
+++ 1.JavaSyntax/src/com/javarush/task/task01/task0109/Solution.java
@@ -0,0 +1,12 @@
+package com.javarush.task.task01.task0109;
+
+/* 
+Объявление и инициализация переменных
+*/
+
+public class Solution {
+    public static void main(String[] args) {
+        //напишите тут ваш код
+        int a = 0, b = 1;
+    }
+}
